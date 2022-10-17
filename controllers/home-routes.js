1255aba42bee58e4f574f3c200bd8184a5bfeecd
brenbrenda55 <108436173+brenbrenda55@users.noreply.github.com>
2022-10-16 21:15:24 -0500

    attempted homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,44 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Content, User, Comment } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
-    res.render('homepage', {
-      id: 1,
-      content: 'test_content',
-      title: 'Handlebars Docs',
-      created_at: new Date(),
-      comments: [{}, {}],
-      user: {
-        username: 'test_user'
-      }
-    });
+    console.log('======================');
+    Post.findAll({
+      attributes: [
+        'id',
+        'content_test',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE content.id)')]
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'content_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+  
+        res.render('homepage', { posts });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   });
+  
   
 
 module.exports = router;
